6d712dade2db733c1df6a1da07b7d71070ae00e5
Shehzad Ahmed <shaxad.here@gmail.com>
2022-11-23 13:36:19 +0500

    init

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react";
+import "./App.css"
+import logos from "./logos";
+import { folders } from "./logos";
+
+
+function App() {
+
+  const [list, setList] = useState([])
+  const [dragInfo, setDragInfo] = useState({
+    oldFolder: "",
+    newFolder: "",
+    item: {}
+  })
+
+  const allowDrop = (ev) => {
+    ev.preventDefault();
+  }
+
+  const drag = (ev, folderName, item) => {
+    setDragInfo({ ...dragInfo, oldFolder: folderName, item: item })
+  }
+
+  const drop = (ev, folderName) => {
+    ev.preventDefault();
+    setDragInfo({ ...dragInfo, newFolder: folderName })
+
+    const _list = list?.map((item) => {
+      if (dragInfo.oldFolder === folderName) {
+        console.log("Dropped inside its old folder")
+        return item
+      }
+      else if (item.folderName === dragInfo.oldFolder) {
+        console.log("removing from old folder")
+        const index = item.logos.findIndex(i => i == dragInfo.item)
+        item.logos.splice(index, 1)
+        return item
+      }
+      else if (item.folderName === folderName) {
+        console.log("adding inside new folder")
+        item.logos.push(dragInfo.item)
+        return item
+      }
+      else {
+        console.log("no action happenned")
+        return item
+      }
+    })
+    setList([..._list])
+  }
+
+  useEffect(() => {
+    const _folders = folders.map((folder) => {
+      const _logos = logos.filter((logo, index) => {
+        if (logo.folderName === folder) {
+          return logo
+        }
+      })
+      return { folderName: folder, logos: _logos }
+    })
+    setList([..._folders])
+  }, [])
+
+  return (
+    <div className="App">
+      <div style={{ display: "flex" }}>
+        {list?.map((item, index) =>
+          <div key={index} style={{ margin: 20 }}>
+            <h1>{item.folderName}</h1>
+            <div className="container" onDrop={(e) => drop(e, item.folderName)} onDragOver={(e) => allowDrop(e)}>
+              {item.logos.map((logo, index) =>
+                <div
+                  key={index}
+                  className="item"
+                  id={logo.title}
+                  data-folder={item.folderName}
+                  data-item={logo.companyName}
+                  draggable="true"
+                  onDragStart={(e) => drag(e, item.folderName, logo)}
+                >
+                  <img src={logo.image} width="336" height="69" />
+                </div>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/logos.js
@@ -0,0 +1,40 @@
+const logos = [
+    {
+        title: "Tostitos",
+        image: "https://bestlifeonline.com/wp-content/uploads/sites/3/2018/09/tostitos.jpg?quality=82&strip=all",
+        folderName: "folder1"
+    },
+    {
+        title: "Noyos",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Logos.svg/1200px-Logos.svg.png",
+        folderName: "folder2"
+    },
+    {
+        title: "Pringles",
+        image: "https://www.boredpanda.com/blog/wp-content/uploads/2018/03/pringles-monopoly-equals-iwd-logo0-5aa67af687436__700.gif",
+        folderName: "folder1"
+    },
+    {
+        title: "Lays",
+        image: "https://logos-world.net/wp-content/uploads/2020/12/Lays-Logo.png",
+        folderName: "folder2"
+    },
+    {
+        title: "Fanta",
+        image: "https://logos-world.net/wp-content/uploads/2020/05/Fanta-Logo.png",
+        folderName: "folder3"
+    },
+    {
+        title: "FedEx",
+        image: "https://image.cnbcfm.com/api/v1/image/102628798-Untitled-4.jpg?v=1430254868&w=720&h=405",
+        folderName: "folder1"
+    },
+
+]
+
+export const folders = [
+    "folder1",
+    "folder2",
+    "folder3"
+]
+export default logos
